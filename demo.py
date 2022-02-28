ca8d5bab866c5f4982e5cc7f17b417cfc610565c
Charlie Turking <Charlie.Turking@gmail.com>
2022-02-28 23:34:16 +0000

    CT Commit new files

---
+++ demo.py
@@ -0,0 +1,52 @@
+import time
+a = 2
+b = 2
+c = a + b
+print(a + b)
+print(c)
+c+=1
+print(c)
+print()   #prints a blank line
+time.sleep(5)
+# Variables and input strings
+apples = input("How many apples are we using today? ")
+oranges = 3
+apples = oranges 
+print("We have " + str(apples)+ " Apples here today")
+print()   #prints a blank line
+time.sleep(5)
+# Lists
+myAnimals = ["cat", "horse", "cat"]  
+print(myAnimals)
+print(type(myAnimals))
+print()   #prints a blank line
+time.sleep(5)
+# For Loops
+for item in myAnimals:
+    print("{} is of the data type {}".format(item,type(item)))
+myAnimals.append("unicorn")
+print(myAnimals.count("cat"))
+print()   #prints a blank line
+time.sleep(5)
+# While Loops
+counter = 0
+while counter <= 4:
+    print("I love looping" + str(counter))
+    counter+=1
+print("done looping at counter = "+ str(counter))
+print()   #prints a blank line
+time.sleep(5)
+# Dictionaries
+myAnimalNames = {"cat1":"Alto", "horse":"Leo", "cat2":"Sopranino", "dog":"Otty"}
+for key, value in myAnimalNames.items():
+    print(key, value)
+print()   #prints a blank line
+time.sleep(5)
+# If 
+horses = 2
+if horses <=1:
+    print("You need MORE horses!")
+elif horses <=2:
+    print("You should consider having more horses")
+else:
+    print("Consider buying another horse")
